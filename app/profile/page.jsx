49d139af02faf79448710ff43878e2c0b39ed488
mikemike27 <mikeymike3193@gmail.com>
2024-06-17 15:57:45 +1000

    Create edit and delete prompt function
    1. Create api update and delete api routes to handle edit and delete prompts, and then save to database
    2. finish all CRUD functionality of the app

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -9,14 +9,30 @@ const MyProfile = () => {
 
     const { data: session } = useSession();
 
+    const router = useRouter();
+
     const [posts, setPosts] = useState([]);
 
-    const handleEdit = ()=>{
-
+    const handleEdit = (post)=>{
+        router.push(`/update-prompt?id=${post._id}`);
     }
 
-    const handleDelete = async()=>{
+    const handleDelete = async(post)=>{
+        const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
 
+        if(hasConfirmed){
+            try {
+                await fetch(`/api/prompt/${post._id.toString()}`, {
+                    method: 'DELETE'
+                });
+
+                const filteredPosts = posts.filter((p)=>p._id !== post._id);
+
+                setPosts(filteredPosts);
+            } catch (error) {
+                console.log(error);
+            }
+        }
     }
 
     const fetchPosts = async ()=>{
@@ -38,7 +54,7 @@ const MyProfile = () => {
         name="My"
         desc="Welcome to your personalized profile page"
         data={posts}
-        hanldeEdit={handleEdit}
+        handleEdit={handleEdit}
         handleDelete={handleDelete}
     />
   )
