67004f42ed4cd5a852238aa791693dd4dbd253eb
mikemike27 <mikeymike3193@gmail.com>
2025-05-19 19:12:10 +1000

    Update route config
    Updated route config

---
+++ app/api/hello/route.config.ts
@@ -0,0 +1,1 @@
+export const schedule = '0 0 * * *';
--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,7 +1,3 @@
-export const route = {
-  schedule: '0 0 * * *', // schedule cron (daily midnight UTC)
-};
-
 export function GET() {
   console.log('Scheduled job ran!');
   return new Response('Hello from Vercel!');
